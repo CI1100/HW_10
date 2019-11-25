73eadc1237a946597b29f1e76ca1ba78e262039c
CI1100 <yev.olga@mail.ru>
2019-11-25 17:41:13 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -11,7 +11,7 @@ setup(
     description="Creation of packages for projects HW8 and HW9",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/CI1100/CEBD-1100-Week-10",
+    url="https://github.com/CI1100/HW_10",
     license= 'LICENSE.txt',
     packages= ['final_project_packages', 'final_project_packages.test'],
     scripts = ['bin/HW_8.py', 'bin/kmeans_iris.py'],
